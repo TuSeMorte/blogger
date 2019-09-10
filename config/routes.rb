b158e5cf41a979bb0623fd7dc92a67a4e45fe51e
TuSeMorte <TuSeMorte@gmail.com>
2019-09-10 09:22:38 -0400

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Blogger::Application.routes.draw do
   resources :articles do
   resources :comments
 end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'articles#index'
+resources :tags
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'articles#index'
 
 end
